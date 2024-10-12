8f1cc70f797e8e7d06c98b2547e1c09f24a3757d
augustqa <augustqabatch@gmail.com>
2024-10-12 08:17:50 +0530

    DebitandCreditcard class added Payment module

---
+++ src/payment/DebitCreditClass.java
@@ -0,0 +1,23 @@
+package payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class DebitCreditClass {
+  @Test
+  public void debitcard() {
+  }
+  
+  @Test
+  public void creditcard() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

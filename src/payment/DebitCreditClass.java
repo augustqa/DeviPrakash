36ddf89f63e8562be258f70bd0696419e578dec5
augustqa <augustqabatch@gmail.com>
2024-10-12 08:21:24 +0530

    Added intial setup for browser launch

--- src/payment/DebitCreditClass.java
+++ src/payment/DebitCreditClass.java
@@ -2,9 +2,12 @@ package payment;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class DebitCreditClass {
+	WebDriver driver;
   @Test
   public void debitcard() {
   }
@@ -14,6 +17,8 @@ public class DebitCreditClass {
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
   @AfterTest

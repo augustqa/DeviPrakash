fdb1731467bfc2ae585dfef82999b970f6753ee5
augustqa <augustqabatch@gmail.com>
2024-10-12 08:35:07 +0530

    Xpath Class added in locators package

---
+++ src/locators/XpathLocatos.java
@@ -0,0 +1,19 @@
+package locators;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class XpathLocatos {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

e06296c8ee83516f613499d2dcf93e993e8b3bb5
augustqa <augustqabatch@gmail.com>
2024-10-12 08:07:13 +0530

    Complete Code  push

---
+++ src/payment/NetBanking.java
@@ -0,0 +1,21 @@
+package payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+
+public class NetBanking {
+	WebDriver driver;
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
